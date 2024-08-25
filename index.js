46a5d8087b45b10f5e52671bf25e6131ca233213
Yello <Isaac Elizan>
2024-08-25 22:51:04 +0800

    a

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
-  intents: [IntentsBitField.Flags.Guilds],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
+
+client.login();
