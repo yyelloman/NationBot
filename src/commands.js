8c0fc57f2791fbe2c89b25bcbdeea27a9aa93033
yyelloman <isaacelizan@proton.me>
2024-08-26 10:48:58 +0000

    rfecdd

--- src/commands.js
+++ src/commands.js
@@ -4,21 +4,25 @@ const path = require("node:path");
 
 const commands = [];
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
 
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
 
-        if ('data' in command && 'execute' in command) {
+        if ("data" in command && "execute" in command) {
             commands.push(command.data.toJSON());
         } else {
-            console.log(`WARNING: The command at ${filePath} is missing a "data" or "execute" property!!`);
+            console.log(
+                `WARNING: The command at ${filePath} is missing a "data" or "execute" property!!`,
+            );
         }
     }
 }
@@ -27,14 +31,18 @@ const rest = new REST().setToken(process.env["TOKEN"]);
 
 (async () => {
     try {
-        console.log(`... registering of ${commands.length} slash commands has BEGUN.`);
+        console.log(
+            `... registering of ${commands.length} slash commands has BEGUN.`,
+        );
 
         const data = await rest.put(
             Routes.applicationCommands(process.env["CLIENT_ID"]),
-            { body: commands }
+            { body: commands },
         );
 
-        console.log(`... registering of ${data.length} slash commands is SUCCESSFUL.`);
+        console.log(
+            `... registering of ${data.length} slash commands is SUCCESSFUL.`,
+        );
     } catch (error) {
         console.error(`ERROR: ${error}`);
     }
