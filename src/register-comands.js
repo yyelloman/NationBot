a438fea91ddcb7b6e950d58ec9c30fa54333ef9e
Yello <Isaac Elizan>
2024-08-26 16:21:44 +0800

    i forgot to save the files

--- src/register-comands.js
+++ src/register-comands.js
@@ -4,13 +4,22 @@ const commands = [
     {
         name: 'hey',
         description: 'replies with hey',
-    }
-]
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env['TOKEN']);
 
 (async () => {
-    try 
+    try {
+        console.log('Registering slash commands');
 
+        await rest.put(
+            Routes.applicationGuildCommands(process.env['CLIENT_ID'], process.env['GUILD_ID']),
+            { body: commands }
+        )
+
+        console.log('Slash commands have been registered correctly')
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-})
+})();
