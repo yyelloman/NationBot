19d0603aa22148013c6f792a83503fa183e702c3
yyelloman <isaacelizan@proton.me>
2024-08-26 18:06:39 +0800

    do not underestimate how stupid i am

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ const client = new Client({
 });
 
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
 
 // when the bot is ready
 client.once(Events.ClientReady, (c) => {
@@ -23,6 +39,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
     if (!command) {
         console.error(`ERROR: Command ${interaction.commandName} does not exist!!`);
+        await interaction.reply(`The console just errored: \`\`\`ERROR: Command ${interaction.commandName} does not exist!!\`\`\``);
         return;
     }
 
