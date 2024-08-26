28b3a1149ee272d4b48caaa76837e3100cafcc46
Yello <Isaac Elizan>
2024-08-26 16:19:38 +0800

    register slash commands

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const { Client, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});           
+
+client.on('ready', (context) => {
+    console.log(`${context.user.tag} is online`);
 });
 
-client.on("ready", (context) => {
-  console.log(`${context.user.tag} is online`);
+client.on('messageCreate', (message) => {
+    console.log(message.content);
 });
 
-client.on("messageCreate", (message) => {
-  console.log(message.content);
-});
-
-client.login(process.env["TOKEN"]);
+client.login(process.env['TOKEN']);
---
+++ src/register-comands.js
@@ -0,0 +1,16 @@
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'replies with hey',
+    }
+]
+
+(async () => {
+    try 
+
+    } catch (error) {
+        console.log(error)
+    }
+})
