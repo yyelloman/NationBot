bf66afa44239637f6a72ab03746593385eb7cd2d
yyelloman <isaacelizan@proton.me>
2024-08-28 14:10:40 +0800

    no need to organize

--- src/commands.js
+++ src/commands.js
@@ -5,25 +5,21 @@ const path = require("node:path");
 const commands = [];
 
 const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+const commandsPath = path.join(foldersPath, folder);
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-        .readdirSync(commandsPath)
-        .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-
-        if ("data" in command && "execute" in command) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.log(
-                `WARNING: The command at ${filePath} is missing a "data" or "execute" property!!`,
-            );
-        }
+    if ("data" in command && "execute" in command) {
+        commands.push(command.data.toJSON());
+    } else {
+        console.log(
+            `WARNING: The command at ${filePath} is missing a "data" or "execute" property!!`,
+        );
     }
 }
 
