e57fc0b00f960f4529186d15d84b8a75839c7e58
yyelloman <isaacelizan@proton.me>
2024-08-28 21:33:45 +0800

    essa

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ChannelType, PermissionFlagsBits, Collection } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ChannelType, PermissionFlagsBits, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require("discord.js");
 const { ACCENT_COLOR, ERROR_COLOR } = require("..");
 
 module.exports = {
@@ -69,11 +69,11 @@ async function proceedWithRegistration(interaction, channel) {
     }
     const startEmbed = new EmbedBuilder()
         .setTitle("Country Registration")
-        .setDescription("Send any message within 3 minutes to begin. You will be given 3 minutes to answer each question.")
+        .setDescription("Send any message within 3 minutes to begin. You will be given 3 minutes to answer each question. You can change your answers later with /settings.")
         .setColor(ACCENT_COLOR);
     await channel.send({ content: `<@${interaction.user.id}>`, embeds: [startEmbed] });
     
-    await channel.awaitMessages({ max: 1, time: 5000, errors: ["time"] })
+    await channel.awaitMessages({ max: 1, time: 180_000, errors: ["time"] })
         .catch(collected => {
             channel.delete(
                 `${interaction.user.username} (${interaction.user.username}) did not give a response in time (registration)`
@@ -89,5 +89,45 @@ async function proceedWithRegistration(interaction, channel) {
         return;
     }
 
-    await channel.send({ content: "working" });
+    const nameEmbed = new EmbedBuilder()
+        .setDescription("**What's the name of your country?** e.g. Canada, Russia, Germany. The full name will be automatically adjusted to the government you choose.")
+        .setColor(ACCENT_COLOR);
+
+    await channel.send({ embeds: embed });
+
+    await channel.awaitMessages({ max: 1, time: 180_000, errors: ["time"] })
+        .catch(collected => {
+            noResponse(interaction, channel);
+            registrationData.ok = false;
+        });
+
+    if (registrationData.ok === false) {
+        return;
+    }
+
+    const governmentEmbed = new EmbedBuilder()
+        .setDescription("**Choose a government type**")
+        .setColor(ACCENT_COLOR);
+    
+    const governmentSelect = new StringSelectMenuBuilder()
+        .setCustomId("government")
+        .setPlaceholder("Choose a government type category")
+        .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setLabel(":1278346684283359233: Democratic")
+                .setDescription("Any form of government where the power is exercised by the people")
+                .setValue("democ"),
+            new StringSelectMenuOptionBuilder()
+                .setLabel("Undemocratic")
+                .setDescription("Any form of government where power belongs to the ")
+                .setValue("undmc")
+        )
+}
+
+function noResponse(interaction, channel) {
+    const noResponseEmbed = new EmbedBuilder()
+        .setDescription("No response was given within 3 minutes. Registration will restart.")
+        .setColor(ERROR_COLOR);
+    channel.send({ embeds: [noResponseEmbed] })
+    proceedWithRegistration(interaction, channel);
 }
