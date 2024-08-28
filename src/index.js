1b734eb5d35f8849a89ec41f8097b9ed29f05be0
yyelloman <isaacelizan@proton.me>
2024-08-28 14:16:41 +0800

    fix something

--- src/index.js
+++ src/index.js
@@ -12,11 +12,12 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-	const filePath = path.join(commandsPath, file);
+const commandsPath = path.join(__dirname, 'commands');
+
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
