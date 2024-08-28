a23a9acea6f44322910d0a0ec60e52e7aa57b227
yyelloman <isaacelizan@proton.me>
2024-08-28 12:49:37 +0800

    fix error

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ for (const folder of commandFolders) {
 }
 
 const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
