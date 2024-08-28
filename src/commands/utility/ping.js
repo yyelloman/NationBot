9d64fcfa20caf4ca165485906a3da07e4fa2ba31
yyelloman <isaacelizan@proton.me>
2024-08-28 12:48:15 +0800

    testing subcommands:

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -3,7 +3,12 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
-        .setDescription("desc"),
+        .setDescription("desc")
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("testsubcommand")
+                .setDescription("test")
+        ),
     async execute(interaction) {
         await interaction.reply("pong");
     }
