9faa3db39c33bdb6f134a012e67bff81394019ed
yyelloman <isaacelizan@proton.me>
2024-08-28 15:55:50 +0800

    first steps of registration

---
+++ src/commands/register.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, EmbedBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require("discord.js");
+const { ACCENT_COLOR } = require("..");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("desc"),
+    async execute(interaction) {
+        const embed = new EmbedBuilder().setDescription(`**Alright, please wait :]**`).setColor(ACCENT_COLOR);
+        await interaction.reply({ embeds: [embed] });
+        const modal = new ModalBuilder()
+            .setCustomId("nameRegistration")
+            .setTitle("Register country: name");
+
+        const nickname = new TextInputBuilder()
+            .setCustomId("nrNickname")
+            .setLabel("Name of country (e.g. Russia)")
+            .setStyle(TextInputStyle.Short);
+        
+        const actionRow = new ActionRowBuilder().addComponents(nickname);
+
+        modal.addComponents(actionRow);
+
+        await interaction.showModal(modal);
+    }
+}
